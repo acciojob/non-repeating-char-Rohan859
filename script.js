7739ab4c2ce47ec37531ddff8fe59cdbc60f5c54
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-12 23:32:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(str) 
+{
  // Write your code here
+	let freq[]=[];
+	for(let ch of str)
+	{
+		let index=ch-'a';
+		freq[index]++;
+	}
+
+	for(let ch of str)
+		{
+			let idx=ch-'a';
+			if(freq[idx]==1)
+			{
+				return ch;
+			}
+		}
+	return null;
 }
+
 const input = prompt("Enter a string");
+
 alert(firstNonRepeatedChar(input)); 
+
